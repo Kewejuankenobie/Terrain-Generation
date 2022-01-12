903ececb029a7bac1a86b4a02f69e821df5fb861
Kewejuankenobie <97452684+Kewejuankenobie@users.noreply.github.com>
2022-01-12 08:51:45 -0500

    Update main.py
    Added a terrain class for creating a terrain object, added the ability to show the noise on a 3d graph, with noise values less than 0 being changed to 0 for where water will be.

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 import perlin_noise
 import matplotlib.pyplot as plt
+import mpl_toolkits
+import numpy as np
 
 #Takes awhile to run
 
 
+class terrain:
+    __pic = []
+    def __init__(self, xBound, yBound, zBound, scale):
+        self.xBound = xBound
+        self.yBound = yBound
+        self.zBound = zBound
+        self.scale = scale
+        noise = perlin_noise.PerlinNoise(octaves=10, seed=4)
+        for i in range(self.xBound):
+            row = []
+            for j in range(self.yBound):
+                noiseNumber = noise([j / self.xBound, i / self.yBound]) * self.scale
+                if noiseNumber < 0.0:
+                    noiseNumber = 0.0
+                row.append(noiseNumber)
+            self.__pic.append(row)
+
+    def printNoise(self):
+        print(self.__pic)
+
+    def createShape(self):
+        fig = plt.figure(figsize=(self.xBound, self.yBound))
+        axes = fig.add_subplot(projection="3d")
+        xArranged = np.arange(0, self.xBound, 1)
+        yArranged = np.arange(0, self.yBound, 1)
+        X, Y = np.meshgrid(xArranged, yArranged)
+        Z = np.array(self.__pic)
+        axes.plot_surface(X, Y, Z)
+        plt.show()
+
+    def colorTerrain(self):
+        pass
 
 def main():
+    terrain1 = terrain(200, 200, 100, 0.25)
+    #terrain1.printNoise()
+    terrain1.createShape()
 
-    noise = perlin_noise.PerlinNoise(octaves=12, seed=5)
-    scale = 0.1
-    x, y, = 256, 256
-    pic = []
-    for i in range(x):
-        row = []
-        for j in range(y):
-            row.append(noise([j/x, i/y]) * scale)
-        pic.append(row)
-    plt.imshow(pic, cmap="gray")
-    plt.show()
+    #plt.imshow(pic, cmap="gray")
 
 if __name__ == '__main__':
     main()
